1b934eeb208adccefcb909911c6d76ebd457b806
David Rouleau <Shabbypenguin@gmail.com>
2013-04-28 02:29:47 -0400

    Moved some stuff to jflte-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,45 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-USE_CAMERA_STUB := true
+
 
 # inherit from the proprietary version
 -include vendor/samsung/jfltespr/BoardConfigVendor.mk
 
-TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := MSM8960
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_BOOTLOADER_BOARD_NAME := jfltespr
-
 # Inherit from qcom-common
--include device/samsung/qcom-common/BoardConfigCommon.mk
+-include device/samsung/jflte-common/BoardConfigCommon.mk
 
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
-BOARD_KERNEL_BASE := 0x80200000
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000
-BOARD_KERNEL_PAGESIZE := 2048
-
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_FLASH_BLOCK_SIZE := 131072
-
+TARGET_BOOTLOADER_BOARD_NAME := jfltespr
 TARGET_PREBUILT_KERNEL := device/samsung/jfltespr/kernel
 
-BOARD_HAS_NO_SELECT_BUTTON := true
-
-# TWRP flags
-DEVICE_RESOLUTION := 1080x1920
-TW_INTERNAL_STORAGE_PATH := "/data/media"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/external_sdcard"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sdcard"
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
-
-# OUDHS Flags
-BOARD_CUSTOM_RECOVERY_SAMSUNG := true
-BOARD_RAINBOW_GRAPHICS := true
+# Assert
+TARGET_OTA_ASSERT_DEVICE := jfltespr
