2a8a24d0c2a07c78b465b43c66c2fa9b1b28bf20
cordell <cordellsmail@gmail.com>
2013-08-13 13:22:53 +0200

    Adjust slim.mk for new bootanimation
    Change-Id: Ic8339e441119243d033858fb4faf3e96085f4d18

--- slim.mk
+++ slim.mk
@@ -18,10 +18,6 @@ $(call inherit-product, vendor/slim/config/common_sgs.mk)
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
-#bootanimation
-PRODUCT_COPY_FILES +=  \
-    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.2.2/JDQ39/L720VPUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.2.2 JDQ39 L720VPUAMDL release-keys" 
 
 ## Device identifier. This must come after all inclusions
