b0981c53c71dffaaa293842114feed01ef364c21
cordell <cordellsmail@gmail.com>
2013-08-29 23:25:02 +0200

    remove common-sgs
    Change-Id: I2d03a83efa7f9e06d67f099521f4f0828df73a70

--- slim.mk
+++ slim.mk
@@ -12,9 +12,6 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 # Inherit some common Slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-# Inherit device settings
-$(call inherit-product, vendor/slim/config/common_sgs.mk)
-
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
