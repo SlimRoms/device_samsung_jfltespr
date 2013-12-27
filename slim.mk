4c77dc1236e122d73c315fd798e8e716577886f1
cordell <cordellsmail@gmail.com>
2013-12-27 11:47:19 +0100

    remove ledflash
    Change-Id: Ic27eacd1fe124677133d6d09b1b7fc402dc6f844

--- slim.mk
+++ slim.mk
@@ -12,9 +12,6 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 # Inherit some common Slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-# Inherit torch settings
-$(call inherit-product, vendor/slim/config/common_ledflash.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.2.2/JDQ39/L720VPUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.2.2 JDQ39 L720VPUAMDL release-keys" 
 
 ## Device identifier. This must come after all inclusions
