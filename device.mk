1c6e35986b2fc0b0eb5ec78f8943e50e0e2bbdef
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-24 08:00:46 -0600

    jfltespr: update props, deps, blobs, de-unify
    Change-Id: I73578bed6f002be5ef6ae49689c40a2fd7f637c2
    
    Conflicts:
    	BoardConfig.mk
    	cm.dependencies
    	cm.mk

--- device.mk
+++ device.mk
@@ -4,8 +4,5 @@ $(call inherit-product-if-exists, vendor/samsung/jfltespr/jfltespr-vendor.mk)
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/jfltespr/overlay
 
-## common overlays
-DEVICE_PACKAGE_OVERLAYS += device/samsung/jf-common/overlay-cdma
-
 # Inherit from jf-common
 $(call inherit-product, device/samsung/jf-common/jf-common.mk)
--- slim.mk
+++ slim.mk
@@ -3,16 +3,17 @@ $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 # Release name
 PRODUCT_RELEASE_NAME := jfltespr
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/cdma.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit some common Slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.2.2/JDQ39/L720VPUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.2.2 JDQ39 L720VPUAMDL release-keys" 
+PRODUCT_BUILD_PROP_OVERRIDES += /
+  PRODUCT_NAME=jfltespr /
+  TARGET_DEVICE=jfltespr /
+  BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.2.2/JDQ39/L720VPUAMDL:user/release-keys" /
+  PRIVATE_BUILD_DESC="jfltespr-user 4.2.2 JDQ39 L720VPUAMDL release-keys" 
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := slim_jfltespr
