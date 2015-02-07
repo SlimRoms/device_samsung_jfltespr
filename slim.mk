6308305842b04745c57c7a687bbddf0e507810e7
nicklovell23 <nicklovell23@gmail.com>
2015-02-07 07:04:23 +0100

    Update fingerprint
    Change-Id: Idfbbf4d04529e31f105915c4a3f86a384fa4c21b

--- slim.mk
+++ slim.mk
@@ -1,20 +1,18 @@
 $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := jfltespr
-
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
-# Inherit some common Slim stuff.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += /
-  PRODUCT_NAME=jfltespr /
-  TARGET_DEVICE=jfltespr /
-  BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.2.2/JDQ39/L720VPUAMDL:user/release-keys" /
-  PRIVATE_BUILD_DESC="jfltespr-user 4.2.2 JDQ39 L720VPUAMDL release-keys" 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=jfltespr \
+    TARGET_DEVICE=jfltespr \
+    BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.4.2/KOT49H/L720VPUFNG2:user/release-keys" \
+    PRIVATE_BUILD_DESC="jfltespr-user 4.4.2 KOT49H L720VPUFNG2 release-keys"
 
-## Device identifier. This must come after all inclusions
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
 PRODUCT_NAME := slim_jfltespr
 PRODUCT_DEVICE := jfltespr
